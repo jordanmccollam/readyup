343f735fcb4bc0e707a3f6b5948d30ad7c08333e
Jordan McCollam <jordy.mccollam@gmail.com>
2019-11-05 12:27:17 -0600

    Model for user profiles

---
+++ models/profile.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {
+  var Profile = sequelize.define("Profile", {
+    // The players gamer tag 
+    // (they will need to be able to edit this in case they switch consoles)
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // Which console the user is currently wanting to play on
+    // They can leave blank if no preference
+    console: {
+      type: DataTypes.STRING
+    },
+    // Call of Duty rank
+    cod_rank: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "not ranked"
+    },
+    // Rocket League rank
+    rl_rank: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "not ranked"
+    },
+    // Fortnite rank
+    fortnite_rank: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "not ranked"
+    },
+    // Where the player decides what game they currently want to play
+    // (Ex: Player could be in the COD room, Rocket League room, or Fortnite room)
+    room: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "waiting"
+    },
+    // If the user is logged in to our site or not
+    loggedin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+  return Profile;
+};
