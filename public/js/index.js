1d501a51e374e82949388f89efdf751f7d57460a
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-11-08 20:39:24 -0600

    added styling, contacts page, title, updated routes for contacts page

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,9 @@ var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
 
+// hide the logout button on the homepage
+$(".btn-secondary-logout").hide();
+
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
